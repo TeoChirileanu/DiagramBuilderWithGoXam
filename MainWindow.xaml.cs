14c35744531fb26122f632b8d9d5eaa35d4e8712
Teodor <teodorchirileanu@thecarpathiancoder.dev>
2020-03-22 21:03:13 +0100

    as easy as it gets

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace GoXam
+﻿using System.Collections.ObjectModel;
+using Northwoods.GoXam.Model;
+
+namespace GoXam
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var startNode = new MyNodeData {Key = "Start"};
+            var endNode = new MyNodeData {Key = "End"};
+
+            var startToEndLink = new MyLinkData {From = startNode.Key, To = endNode.Key};
+
+            var model = new GraphLinksModel<MyNodeData, string, string, MyLinkData>
+            {
+                NodesSource = new ObservableCollection<MyNodeData> {startNode, endNode},
+                LinksSource = new ObservableCollection<MyLinkData> {startToEndLink}
+            };
+
+            MyDiagram.Model = model;
         }
     }
 }
