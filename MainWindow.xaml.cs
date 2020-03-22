b775d3a36cc54a986c91328008bf9e90a61412b1
Teodor <teodorchirileanu@thecarpathiancoder.dev>
2020-03-22 21:48:39 +0100

    add palette and refactor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,26 +1,31 @@
 ﻿using System.Collections.ObjectModel;
-using Northwoods.GoXam.Model;
 
 namespace GoXam
 {
     public partial class MainWindow
     {
+        private static readonly MyNode StartNode = new MyNode {Key = "Start"};
+        private static readonly MyNode EndNode = new MyNode {Key = "End"};
+        private static readonly MyLink StartToEndLink = new MyLink {From = StartNode.Key, To = EndNode.Key};
+
+        private readonly ObservableCollection<MyNode> _nodes = new ObservableCollection<MyNode>{StartNode, EndNode};
+        private readonly ObservableCollection<MyLink> _links = new ObservableCollection<MyLink> {StartToEndLink};
+
         public MainWindow()
         {
             InitializeComponent();
 
-            var startNode = new MyNodeData {Key = "Start"};
-            var endNode = new MyNodeData {Key = "End"};
-
-            var startToEndLink = new MyLinkData {From = startNode.Key, To = endNode.Key};
-
-            var model = new GraphLinksModel<MyNodeData, string, string, MyLinkData>
+            MyDiagram.Model = new MyModel
             {
-                NodesSource = new ObservableCollection<MyNodeData> {startNode, endNode},
-                LinksSource = new ObservableCollection<MyLinkData> {startToEndLink}
+                NodesSource = _nodes,
+                LinksSource = _links,
+                Modifiable = true
             };
 
-            MyDiagram.Model = model;
+            MyPalette.Model = new MyModel
+            {
+                NodesSource = _nodes
+            };
         }
     }
 }
